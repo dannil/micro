a92aa9355d450cedadf9c1391492b51712a8456e
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2023-11-06 20:59:10 +0100

    tech: setup basic test environment

--- src/test/java/com/github/dannil/demo/DemoApplicationTests.java
+++ src/test/java/com/github/dannil/demo/DemoApplicationTests.java
@@ -1,13 +1,48 @@
 package com.github.dannil.demo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
+
+	public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+		DockerImageName.parse("postgres:16")
+	);
+
+	public static KafkaContainer kafka = new KafkaContainer(
+		DockerImageName.parse("confluentinc/cp-kafka:7.5.1")
+	);
+
+	@DynamicPropertySource
+	public static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+		registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+	}
+
+	@BeforeAll
+	public static void beforeAll() {
+		postgres.start();
+		kafka.start();
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		postgres.stop();
+		kafka.stop();
+	}
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
