7e4d3622fe776a779acfb6182c769d7f8aa3b86c
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2023-11-14 19:50:18 +0100

    tech: use Testcontainers JUnit extension

--- person-service/src/test/java/com/github/dannil/springgraphql/personservice/PersonServiceAppTests.java
+++ person-service/src/test/java/com/github/dannil/springgraphql/personservice/PersonServiceAppTests.java
@@ -1,22 +1,25 @@
 package com.github.dannil.springgraphql.personservice;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers(parallel = true)
 public class PersonServiceAppTests {
 
+	@Container
 	public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
 		DockerImageName.parse("postgres:16")
 	);
 
+	@Container
     public static RabbitMQContainer rabbitMQ = new RabbitMQContainer(
 		DockerImageName.parse("rabbitmq:3-management")
     );
@@ -30,18 +33,6 @@ public class PersonServiceAppTests {
 		registry.add("spring.rabbitmq.port", rabbitMQ::getAmqpPort);
 		registry.add("spring.rabbitmq.username", rabbitMQ::getAdminUsername);
 		registry.add("spring.rabbitmq.password", rabbitMQ::getAdminPassword);
-	}
-
-	@BeforeAll
-	public static void beforeAll() {
-		postgres.start();
-		rabbitMQ.start();
-	}
-
-	@AfterAll
-	public static void afterAll() {
-		postgres.stop();
-		rabbitMQ.stop();
 	}
 
 	@Test
