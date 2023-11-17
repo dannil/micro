867bdebac8434a1e631c27b09f0a711b592e2f56
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2023-11-17 22:10:17 +0100

    wip: product

---
+++ product-service/src/test/java/com/github/dannil/micro/productservice/ProductServiceAppTests.java
@@ -0,0 +1,13 @@
+package com.github.dannil.micro.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceAppTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
